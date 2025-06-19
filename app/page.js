adf0ad49a1a739ac6c55fcbbc9c27b6420d974aa
joe14r <yousseframez282@gmail.com>
2025-06-19 14:42:21 +0800

    debloyment error

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 "use client";
-
+import { useEffect, useState } from "react";
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import Blog from "./components/homepage/blog";
@@ -11,24 +11,24 @@ import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
 async function getData() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
-
+  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`);
   if (!res.ok) {
-    throw new Error('Failed to fetch data')
+    throw new Error('Failed to fetch data');
   }
-
   const data = await res.json();
-
   const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
-
   return filtered;
-};
+}
 
-export default async function Home() {
-  const blogs = await getData();
+export default function Home() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    getData().then(setBlogs).catch(console.error);
+  }, []);
 
   return (
-    <div suppressHydrationWarning >
+    <div suppressHydrationWarning>
       <HeroSection />
       <AboutSection />
       <Experience />
@@ -38,5 +38,5 @@ export default async function Home() {
       <Blog blogs={blogs} />
       <ContactSection />
     </div>
-  )
-};
+  );
+}
