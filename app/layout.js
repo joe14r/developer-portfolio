15433a40825d5debd793ae5a65327085bb5a6313
joe14r <yousseframez282@gmail.com>
2025-06-21 10:00:08 +0800

    Update portfolio title to Youssef Arafat

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ import ClientOnlyLayout from "./components/ClientOnlyLayout";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Portfolio of Abu Said - Software Developer",
+  title: "Portfolio of Youssef Arafat - Software Developer",
   description:
     "This is the portfolio of Abu Said. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
 };
